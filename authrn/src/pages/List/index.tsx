ca5b4fc9e214b7c43bcf7ea3225d6f32410a7ea3
GledsonS831 <glegogle84@gmail.com>
2020-07-15 15:48:38 -0300

    :pencil: add text

--- authrn/src/pages/List/index.tsx
+++ authrn/src/pages/List/index.tsx
@@ -4,9 +4,18 @@ import * as St from './styles';
 
 const List: React.FC = () => {
   return (
-    <View style={St.styles.container}>
-      <Text style={St.styles.title}>asdfas</Text>
-    </View>
+    <>
+      <View style={St.styles.container}>
+        <Text style={St.styles.title}>asdfas</Text>
+      </View>
+      <View style={St.page.container}>
+        <Text style={St.flattenStyle}>React Native</Text>
+        <Text>Flatten Style</Text>
+        <Text style={St.page.code}>
+          {JSON.stringify(St.flattenStyle, null, 2)}
+        </Text>
+      </View>
+    </>
   );
 };
 
--- authrn/src/pages/List/styles.ts
+++ authrn/src/pages/List/styles.ts
@@ -19,3 +19,33 @@ export const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
+
+export const page = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    alignItems: 'center',
+  },
+  text: {
+    color: '#000',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  code: {
+    marginTop: 12,
+    padding: 12,
+    borderRadius: 8,
+    color: '#666',
+    backgroundColor: '#eaeaea',
+  },
+});
+
+export const typography = StyleSheet.create({
+  header: {
+    color: '#61dafb',
+    fontSize: 30,
+    marginBottom: 36,
+  },
+});
+
+export const flattenStyle = StyleSheet.flatten([page.text, typography.header]);
